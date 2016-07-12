c7a68b4684ed5fc4de6249195dc35a9440aea5aa
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-12 01:28:28 -0500

    variable

---
+++ app.js
@@ -0,0 +1,14 @@
+/********************************************************
+*                   JavaScript Objects                  *
+* Objects           Properties              Methods     *
+* car               car.name = Fiat         car.start() *
+*                   car.model = 500         car.drive() *
+*                   car.weight = 850kg      car.brake() *
+*                   car.color = white       car.stop()  *
+* All cars have the same properties, but the property   *
+* values differ from car to car.                        *
+* All cars have the same methods, but the methods are   *
+* performed at different times.                         *
+*********************************************************/
+var car = "Fiat";
+document.getElementById("demo").innerHTML = car;
